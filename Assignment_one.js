cc1aa3eb454e1df362ee093e1a9a82903de5d991
Feliciaomollo <48946836+andyomollo@users.noreply.github.com>
2026-03-09 08:41:34 +0300

    added switch statement to control flow

---
+++ Assignment_one.js
@@ -0,0 +1,32 @@
+// A customer wants to order an alcoholic coffee drink
+// If they are 18 or older, allow the order
+// If they are under 18, deny it
+
+let age = 16; 
+
+
+if (age >= 18){
+    console.log("Please place your order")
+}else{
+    console.log("Sorry, you must be 18 or older to order this drink.")
+}
+
+// A customer picks a size: "small", "medium", or "large"
+// Print the price for each size
+// If unknown size, print "Invalid size"
+
+let size = "medium"; 
+
+switch(size){
+    case "Medium":
+        console.log("Medium coffee costs $4")
+        break;
+    case "Small":
+        console.log("Small coffee costs $1")
+        break;
+    case "Large":
+        console.log("Large coffee costs $9")
+        break;
+    default:
+        console.log("Invalid size")
+}
