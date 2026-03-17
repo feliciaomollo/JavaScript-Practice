40e21396157cceb38b469c9c554dd10c9ca3e538
Feliciaomollo <48946836+andyomollo@users.noreply.github.com>
2026-03-17 09:11:03 +0300

    updated conditionsals.js

--- conditionals.js
+++ conditionals.js
@@ -8,3 +8,51 @@ while (i) { // when i becomes 0, the condition becomes falsy, and the loop stops
   alert( i );
   i--;
 }
+let i = 0;
+do {
+  alert( i );
+  i++;
+} while (i < 3);
+
+for (let i = 0; i < 3; i++) { // shows 0, then 1, then 2
+  alert(i);
+}
+
+//inline variable
+for (let i = 0; i < 3; i++) {
+  alert(i); // 0, 1, 2
+}
+alert(i); // error, no such variable
+
+let i = 0;
+
+for (i = 0; i < 3; i++) { // use an existing variable
+  alert(i); // 0, 1, 2
+}
+
+alert(i); // 3, visible, because declared outside of the loop
+
+//skipping parts
+let i = 0; // we have i already declared and assigned
+
+for (; i < 3; i++) { // no need for "begin"
+  alert( i ); // 0, 1, 2
+}
+
+let i = 0;
+
+for (; i < 3;) {
+  alert( i++ );
+}
+
+let sum = 0;
+
+while (true) {
+
+  let value = +prompt("Enter a number", '');
+
+  if (!value) break; // (*)
+
+  sum += value;
+
+}
