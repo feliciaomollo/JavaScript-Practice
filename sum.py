9951bd3e04b00078289c2f7a326f3ec9388cb100
Feliciaomollo <48946836+andyomollo@users.noreply.github.com>
2026-04-13 20:48:21 +0300

    added a python file

---
+++ sum.py
@@ -0,0 +1,6 @@
+Number1 = int (input("Enter value of First number: "))
+Number2 = int(input("Enter value of Second number: "))
+
+sum = (Number1 + Number2)
+
+print("The sum of the two numbers is:" , sum)
