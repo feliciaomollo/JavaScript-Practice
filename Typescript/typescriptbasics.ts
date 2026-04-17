fcabb00f2c26f9962f5b91076cda0bc61951adfe
Feliciaomollo <48946836+andyomollo@users.noreply.github.com>
2026-04-17 09:39:20 +0300

    update

--- Typescript/typescriptbasics.ts
+++ Typescript/typescriptbasics.ts
@@ -5,3 +5,35 @@ console.log(message);
 const age: number = 25;
 
 console.log(age);
+
+
+let count = 10;
+
+if(count > 5){
+    let message: string = "The count has begun"; //let gives message block-level scope, meaning it's only available within the if block.
+    console.log(message)
+}
+
+let userName: string = "Arjun";  
+let age: number = 25;            
+let isActive: boolean = true;    
+
+function greetUser(name: string, age: number): string { //The function must return a string
+  return `Hello, ${name}! You are ${age} years old.`;
+}
+
+let greeting = greetUser(userName, age);
+console.log(greeting);
+
+//Local scope
+function testLocalScope() {
+  let localVar = "I am local";
+  console.log(localVar);  
+}
+
+//Global scope
+let globalVar = 10;  
+function displayGlobalVar() {
+  console.log(globalVar); 
+}
+displayGlobalVar();
