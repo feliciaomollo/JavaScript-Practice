87db4b7edfe06255b2a4b168c57d26f44724b3b2
Feliciaomollo <48946836+andyomollo@users.noreply.github.com>
2026-04-23 09:48:44 +0300

    updated functions

--- Typescript/functions.ts
+++ Typescript/functions.ts
@@ -5,7 +5,16 @@ function addSum(num:number){ //add a number so that it doesnt pick any cause it 
 function getUpper(val : string){
     return val.toUpperCase()
 }
+
+function signIn(name : string, email : string,isPaid :boolean){
+    return name.toUpperCase
+}
+
+let loginUser = (name : string, email : string, isPaid :boolean) =>{}
+
 addSum(50)
 getUpper("water")
+signIn("Fele","fele@gmail.com", true)
+loginUser("Fels", "fels@yahoo.com", true)
 
 export{}
