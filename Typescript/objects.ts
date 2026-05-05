66e92ab6f3ac51972a5105165d98029b63c586d9
Feliciaomollo <48946836+andyomollo@users.noreply.github.com>
2026-05-05 10:40:12 +0300

    created objects

---
+++ Typescript/objects.ts
@@ -0,0 +1,11 @@
+function createUser({name: string, isPaid: boolean}){
+
+}
+createUser({name: "Andeezy", isPaid: true})
+
+
+function createCourse(): {name: string, price: number}{
+    return {name: "Andy", price: 899}
+}
+
+export{}
