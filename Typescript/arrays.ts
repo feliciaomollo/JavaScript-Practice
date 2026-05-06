d0acdfe993270846dbb23b436ba9174da1c4afc9
Feliciaomollo <48946836+andyomollo@users.noreply.github.com>
2026-05-06 10:53:36 +0300

    added arrays.ts

--- Typescript/arrays.ts
+++ Typescript/arrays.ts
@@ -1,3 +1,22 @@
+// Two ways to declare an array
+
+// Way 1 — Type[]
+let fruits: string[] = ["Apple", "Banana", "Mango"]
+let numbers: number[] = [1, 2, 3, 4, 5]
+let flags: boolean[] = [true, false, true]
+
+// Way 2 — Array<Type>
+let fruits: Array<string> = ["Apple", "Banana", "Mango"]
+
+
 const superHeroes: string[] = []//syntax so that we dont get never. this lets it know its a string array.
 
 superHeroes.push("Starlight")
+
+
+//accessing items
+let fruits: string[] = ["Apple", "Banana", "Mango"]
+
+console.log(fruits[0])  // Apple
+console.log(fruits[1])  // Banana
+console.log(fruits[2])  // Mango
