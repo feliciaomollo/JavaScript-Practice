f0ede9a7389fb85654abdb6e7d4c2aa6aa935fe2
Feliciaomollo <48946836+andyomollo@users.noreply.github.com>
2026-05-18 11:51:23 +0300

    updated class.ts

--- Typescript/class.ts
+++ Typescript/class.ts
@@ -14,9 +14,29 @@ const bubba = new User("b@bubbs.com", 'bubba')
 class User1{
     
     constructor(
-        public email: string,
+        public email: string, //this removes dependancy on using this.
         private name: string
         ){}
+        get emailID():string{
+            return `id${this.email}`
+        }
 }
 
 const bubb = new User1("b@bubbs.com", 'bubba')
+
+class Person {
+    name: string
+    age: number
+
+    constructor(name: string, age: number) { //constructor Used to initialize properties with values
+        this.name = name
+        this.age = age
+    }
+
+    greet(): void {
+        console.log(`Hi, I am ${this.name} and I am ${this.age} years old`)
+    }
+}
+
+const person1 = new Person("Arjun", 25)
+person1.greet()  // Hi, I am Arjun and I am 25 years old
