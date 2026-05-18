0c02ac8dd17e7aed5b3ca7b0a4882c79fc16395e
Feliciaomollo <48946836+andyomollo@users.noreply.github.com>
2026-05-18 10:17:57 +0300

    added class.ts

--- Typescript/class.ts
+++ Typescript/class.ts
@@ -1,9 +1,22 @@
 class User{
     email: string
     name: string
+    readonly city = "Nairobi"
     constructor(email: string, name: string){
-        this.email = email;
+        this.email = email; //this is commonly used in javascript
         this.name = name
     }
 }
 
+const bubba = new User("b@bubbs.com", 'bubba')
+
+//private and public
+class User1{
+    
+    constructor(
+        public email: string,
+        private name: string
+        ){}
+}
+
+const bubb = new User1("b@bubbs.com", 'bubba')
