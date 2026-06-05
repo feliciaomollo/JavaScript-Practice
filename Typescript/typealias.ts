9353536c0ebc9c1eed1ca93ce02bb25323630f4d
Feliciaomollo <48946836+andyomollo@users.noreply.github.com>
2026-06-05 15:33:14 +0300

    updated type alias

--- Typescript/typealias.ts
+++ Typescript/typealias.ts
@@ -12,3 +12,24 @@ type User = {
 
 function getUser(): User {}
 function updateUser(): User {}
+
+
+type Point = {
+    x: number;
+    y: number;
+};
+
+type Shape = "circle" | "square" | "rectangle";
+
+function drawShape(shape: Shape, position: Point): void {
+    console.log(`Drawing a ${shape} at (${position.x}, ${position.y})`);
+}
+
+drawShape("circle", { x: 10, y: 20 });
+
+//alias for union type
+type UserId = number | string;
+
+let Id: UserId;
+Id = 'hsgh',
+Id = 123
